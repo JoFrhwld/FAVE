b0899498596e63a42b2d01fe540c443d47e70c2a
Josef Fruehwald <JoFrhwld@gmail.com>
2014-06-03 10:25:10 +0100

    refactored to remove an uneccessary math.exp that was causing problems

--- FAVE-extract/bin/extractFormants.py
+++ FAVE-extract/bin/extractFormants.py
@@ -1720,9 +1720,9 @@ def predictF1F2(phone, selectedpoles, selectedbandwidths, means, covs):
                     # (if F3 and bandwidth measurements exist, add to list of appended values)
                     if len(poles) > 2:
                         values.append(
-                            [x[0], x[1], x[2], x[3], poles[2], bandwidths[2]])
+                            [poles[i], poles[j], bandwidths[i], bandwidths[j], poles[2], bandwidths[2]])
                     else:
-                        values.append([x[0], x[1], x[2], x[3], '', ''])
+                        values.append([poles[i], poles[j], bandwidths[i], bandwidths[j], '', ''])
                     # append corresponding Mahalanobis distance to list of
                     # distances
                     distances.append(dist)
@@ -1745,11 +1745,11 @@ def predictF1F2(phone, selectedpoles, selectedbandwidths, means, covs):
     # if there is a "gap" in the wave form at the point of measurement, the bandwidths returned will be empty,
     # and the following will cause an error...
     if values[winnerIndex][2]:
-        b1 = math.exp(values[winnerIndex][2])
+        b1 = values[winnerIndex][2]
     else:
         b1 = ''
     if values[winnerIndex][3]:
-        b2 = math.exp(values[winnerIndex][3])
+        b2 = values[winnerIndex][3]
     else:
         b2 = ''
     if values[winnerIndex][5]:
